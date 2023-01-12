dc278179b20a0681f25c50ff9d220a0f05315b57
EvGen Gal <genwolk1@gmail.com>
2023-01-13 02:47:57 +0300

    созд.табл.PwSh,проверки

--- bd.js
+++ bd.js
@@ -1,8 +1,15 @@
+// конфиг.БД
+
 // подкл.`общий фонд` из pg
 const Pool = require("pg").Pool;
-// созд.объ.кл.Pool для запросов к БД, куда передаём объ. для настр.
+// созд.объ.кл.Pool для запросов к БД, куда передаём объ.для настр.
 const pool = new Pool({
-  // польз.подкл.к БД,пароль от СУБД,host,port по умолч при устан 5432,назв.БД
+  // польз.подкл.к БД(postgres),пароль от СУБД,host,port по умолч при устан 5432,назв.БД
+  user: "postgres",
+  password: "Qaz123PoS!",
+  host: "localhost",
+  port: "5432",
+  database: "node_postgres",
 });
 
 module.exports = pool;
--- database.sql
+++ database.sql
@@ -1,1 +1,15 @@
 -- sql запросы для созд.табл.
+-- созд.табл.
+create TABLE person(
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(255),
+  surname VARCHAR(255)
+);
+
+create TABLE post(
+  id SERIAL PRIMARY KEY,
+  title VARCHAR(255),
+  content VARCHAR(255),
+  user_id INTEGER,
+  FOREIGN KEY (user_id) REFERENCES person (id)
+);
