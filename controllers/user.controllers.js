1e42ecd159574b226baccee2dcfe7371205a2e24
EvGen Gal <genwolk1@gmail.com>
2023-01-13 18:38:22 +0300

    UserControllers пропис.запр. GET,PUT,UPDATE,DELETE

--- controllers/user.controllers.js
+++ controllers/user.controllers.js
@@ -1,4 +1,4 @@
-// fn|запросы  по взаимодейств.с польз.
+// fn|запросы по взаимодейств.с польз.
 
 // подкл.конфиг.БД для записи получ.данн.в БД
 const bd = require("../bd");
@@ -12,7 +12,7 @@ const { validationResult } = require("express-validator");
 
 // обьяв.кл.(для компановки) с неск.мтд
 class UserControllers {
-  // созд.польз.
+  // async созд.польз. по SQL запросу ВСТАВКИ, /* валидации, шифр.пароля, */ проверками
   async createUser(req, res) {
     // базов.логика с обраб.ошб.
     try {
@@ -61,100 +61,57 @@ class UserControllers {
       });
     }
   }
-  // возрат всех польз.
+
+  // async возрат всех польз. с SQL запросом ПОЛУЧЕНИЯ ВСЕХ
   async getUser(req, res) {
     try {
-      res.json("getUsers");
+      const allUser = await bd.query(`SELECT * FROM person`);
+      // возвращ. весь массив
+      res.json(allUser.rows);
     } catch (error) {}
   }
-  // возврат юзера по id
-  async getOneUser(req, res) {}
-  // обнов.данн.польз.
-  async updateUser(req, res) {}
-  // удал.данн.польз.
-  async deleteUser(req, res) {}
 
-  // // ^ Сделать  проверку по UlbiTV
-  // async register(req, res) {
-  //   // базов.логика с обраб.ошб.
-  //   try {
-  //     // проверка вход.полей на валидацию
-  //     const errorsValid = validationResult(req);
-  //     // е/и проверка не прошла(не пусто) - возвращ.Ответ на front смс ошб.(кастомизируем) + errors.масс.
-  //     if (errorsValid.isEmpty()) {
-  //       return res.status(400).json({
-  //         errors: errorsValid.array(),
-  //         message: "Некорректые данные при регистрации",
-  //       });
-  //     }
+  // async ПОЛУЧЕНИЯ по ID с SQL
+  async getOneUser(req, res) {
+    try {
+      // из парам.запр.получ.id
+      const id = req.params.id;
+      // получ.по id
+      const idUser = await bd.query(`SELECT * FROM person WHERE id = $1`, [id]);
+      // возвращ.только польз.(rows) на клиента
+      res.json(idUser.rows[0]);
+    } catch (error) {}
+  }
 
-  //     // получ.данн.с fronta
-  //     const { email/* , password */ /* , fullName, avatarUrl */ } = req.body;
+  // async ОБНОВЛЕНИЯ данн.польз. с SQL
+  async updateUser(req, res) {
+    try {
+      // получ.все данн.с fronta
+      const { id, name, surname, email /* , password */ } = req.body;
+      // измен.поля, возвращ.т.к. UPDATE этого не делает
+      const updUser = await bd.query(
+        `UPDATE person set name = $1, surname = $2, email = $3 WHERE id = $4 RETURNING *`,
+        [name, surname, email, id /* , password */ /* hashedPassword */]
+      );
+      // возвращ.только польз.(rows) на front
+      res.json(updUser.rows[0]);
+    } catch (error) {}
+  }
 
-  //     // ЛОГИКА регистр.нов.польз. + проверки
-  //     // ^ сделать проверку для fullName|userName
-
-  //     // проверка существ.email по совпад.ключ и значен.
-  //     const candidate = await User.findOne({ email });
-  //     // е/и польз.есть - возвращ.Ответ в смс ошб.
-  //     if (candidate) {
-  //       return res
-  //         .status(400)
-  //         .json({ message: `Такой пользователь уже есть - ${candidate}.` });
-  //     }
-
-  //     // `ждём` hashирование/шифрование пароля ч/з bcryptjs. 1ый пароль, 2ой степень шифр.
-  //     // const salt = await bcrypt.getSalt(12);
-  //     const hashedPassword = await bcrypt.hash(password, 12 /* salt */);
-
-  //     // СОЗД.НОВ.ПОЛЬЗОВАТЕЛЯ (пароль совпад.с шифрованым)
-  //     const user = new User({
-  //       email,
-  //       password: hashedPassword,
-  //       // fullName,
-  //       // avatarUrl,
-  //     });
-
-  //     // ждём сохран.от польз.
-  //     await user.save();
-
-  //     // ^ TOKEN
-  //     // ! ЗДЕСЬ ИЛИ РЯДОМ по https://www.youtube.com/watch?v=d_aJdcDq6AY ИЛИ https://www.youtube.com/watch?v=GQ_pTmcXNrQ&t=935s
-
-  //     // promis завершён,польз.созд.в БД. Ответ fronty смс
-  //     res.status(201).json({
-  //       message: `Пользователь с email ${email} создан и зарегистрирован`,
-  //     });
-  //     // fullName ${fullName} и
-  //   } catch (error) {
-  //     // общ.отв. на серв.ошб. в json смс
-  //     res
-  //       .status(500)
-  //       .json({ message: `Не удалось зарегистрироваться - ${error}.` });
-  //   }
-  // }
-
-  // // ^ Прописать логику для login ВМ + UlbiTV
-  // async login(req, res) {
-  //   try {
-  //     res.json("login");
-  //   } catch (error) {}
-  // }
-
-  // async getUsers(req, res) {
-  //   try {
-  //     // кастыль для созд. ролей в БД
-  //     // !!! Ошб. UlbiTV при подкл. к MongoDB (откл.в России)
-  //     // ! Ошибка сервера: неверная аутентификация: не удалось выполнить аутентификацию.
-  //     /* // ! Не удалось отправить запрос Ошибка: подключение ECONNREFUSED 127.0.0.1:3005 */
-  //     // const userRole = new Role();
-  //     // const adminRole = new Role({ value: "ADMIN" });
-  //     // await userRole.save();
-  //     // await adminRole.save();
-  //     // ^ Раб в get Postman
-  //     res.json("getUsers 33333");
-  //   } catch (error) {}
-  // }
+  // async УДАЛЕНИЕ данн.польз. с SQL
+  async deleteUser(req, res) {
+    try {
+      // из парам.запр.получ.id
+      const id = req.params.id;
+      const { name, surname, email /* , password */ } = req.body;
+      // удал.по id
+      const idUser = await bd.query(`DELETE FROM person WHERE id = $1`, [id]);
+      // возвращ.только польз.(rows) на клиента
+      // res.json(idUser.rows[0]);
+      // ИЛИ сообщ. что удалён
+      res.json(`Пользователь удалён  ${name} ${surname}.`);
+    } catch (error) {}
+  }
 }
 
 // экспорт объ.кл.
