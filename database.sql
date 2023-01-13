8d102297de1bbe57150ab85c1a1af57180b15c96
EvGen Gal <genwolk1@gmail.com>
2023-01-13 18:33:07 +0300

    мини коррект.SQL запроса и userRoutes

--- database.sql
+++ database.sql
@@ -10,6 +10,6 @@ create TABLE post(
   id SERIAL PRIMARY KEY,
   title VARCHAR(255),
   content VARCHAR(255),
-  user_id INTEGER,
-  FOREIGN KEY (user_id) REFERENCES person (id)
+  userId INTEGER,
+  FOREIGN KEY (userId) REFERENCES person (id)
 );
