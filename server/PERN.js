803be8f63fb8e9ceb5a583738c50061f6b5ed6ff
EvGen Gal <genwolk1@gmail.com>
2023-01-15 04:27:24 +0300

    PERN.js (start,вызов sequelize.authenticate,.sync)

--- server/PERN.js
+++ server/PERN.js
@@ -1,24 +1,29 @@
-// Запуск Сервера. Баз.конфиг для приёма запросов
+// Запуск Сервера. Базов.конфиг для приёма запросов
 
+// подкл.ф.наст.конфигураций
+// const config = require("config");
+require("dotenv").config();
 // получ express ч/з require для прилож.
 const express = require("express");
-// // подкл.ф.наст.конфигураций
-// const config = require("config");
+// подкл.конфиг.БД
+const sequelize = require("./db");
+
 // подкл. ф.настр.маршрутов
-const userRoutes = require("./routes/user.routes");
-const postRoutes = require("./routes/post.routes");
+// const userRoutes = require("./routes/user.routes");
+// const postRoutes = require("./routes/post.routes");
 // // подкл. mongoDB ч/з mongoose для базы данных
 // const mongoose = require("mongoose");
 // // от ошибки устаревшего кода
 // mongoose.set("strictQuery", false);
 
 // в конст PORT запис.порт из config (сист.перем.) или 5000
-const PORT = /* config.get("port") */ process.env.PORT || 5005;
+// const PORT = config.get("port") || 7531;
+const PORT = process.env.PORT || 7531;
 
 // созд.server
 const app = express();
 // парсим json сервером
-app.use(express.json());
+// app.use(express.json());
 // ! от ошб
 // app.use(
 //   express.urlencoded({
@@ -26,8 +31,8 @@ app.use(express.json());
 //   })
 // );
 // прослуш. маршруты для обраб.запросов с fronta. 1ый str. префикс для пути(/api), 2ой подкл. Маршрутизатор(middleware)
-app.use("/PERN", userRoutes);
-app.use("/PERN", postRoutes);
+// app.use("/PERN", userRoutes);
+// app.use("/PERN", postRoutes);
 
 // тест1
 // app.get("/", (req, res) => {
@@ -40,52 +45,58 @@ app.use("/PERN", postRoutes);
 //   );
 // });
 // `прослушка` сервера на PORT c fn колбэк cg при успехе,провер.err
-app.listen(PORT, (err) => {
-  if (err) {
-    return console.log(err);
-  }
-  console.log(`Сервер запущен на порту ${PORT}.....`);
-});
-
-// // парсим json сервером
-// app.use(express.json());
-// // прослуш. маршруты для обраб.API запросов с fronta. 1ый str. префикс для пути(/api/auth), 2ой подкл. middleware
-// app.use("/auth", authRoutes /* require("./routes/auth.routes") */);
-
-// // Запуск Сервера | const start = () => {}. fn обёртк. для mongoose
-// async function start() {
-//   // всё верно
-//   try {
-//     // подкл. к БД. await для ожидан. промиса. Вызов mongoose.мтд.connect. 1ый парам url адрес с БД(из config), 2ой парам. набор опций
-//     await mongoose.connect(config.get("mongoUri"), {
-//       // парам из видео для успешн.connect
-//       useNewUrlParser: true,
-//       useUnifiedTopology: true,
-//       // useCreateIndex: true,
-//     });
-// логика приложен.
-// маршр.получ.запроса на гл.стр. В Ответ h5 с текстом
-// app.get("/", (req, res) => {
-//   res.send(
-//     `<body style='text-align: center;;font-family: sans-serif;background:#4b0000'>
-//       <h1 style='font-weight: 900'>Старт PERN 0.0.2</h1>
-//       <p style='font-weight: 900;'>PostgreSQL, Express, React, NodeJS</p>
-//       <p>%{ PORT }</p>
-//     </body>`
-//   );
-// });
-// // `прослушка` сервера на PORT c fn колбэк cg при успехе,провер.err
 // app.listen(PORT, (err) => {
 //   if (err) {
 //     return console.log(err);
 //   }
 //   console.log(`Сервер запущен на порту ${PORT}.....`);
 // });
-//   } catch (error) {
-//     // отраб.ошб.
-//     console.log("Ошибка сервера", error.message);
-//     // выход ч/з глоб.проц.мтд .exit
-//     process.exit(1);
-//   }
-// }
-// start();
+
+// // парсим json сервером
+// app.use(express.json());
+// // прослуш. маршруты для обраб.API запросов с fronta. 1ый str. префикс для пути(/api/auth), 2ой подкл. middleware
+// app.use("/auth", authRoutes /* require("./routes/auth.routes") */);
+
+// Запуск Сервера | const start = async () => {}
+// async function start() {
+const start = async () => {
+  //   //   // всё верно
+  try {
+    // подкл. к БД.
+    await sequelize.authenticate();
+    // сверка сост.БД со схемой данн. ~
+    await sequelize.sync();
+
+    //     //     await mongoose.connect(config.get("mongoUri"), {
+    //     //       // парам из видео для успешн.connect
+    //     //       useNewUrlParser: true,
+    //     //       useUnifiedTopology: true,
+    //     //       // useCreateIndex: true,
+    //     //     });
+    //     // логика приложен.
+    //     // маршр.получ.запроса на гл.стр. В Ответ h5 с текстом
+    //     // app.get("/", (req, res) => {
+    //     //   res.send(
+    //     //     `<body style='text-align: center;;font-family: sans-serif;background:#4b0000'>
+    //     //       <h1 style='font-weight: 900'>Старт PERN 0.0.2</h1>
+    //     //       <p style='font-weight: 900;'>PostgreSQL, Express, React, NodeJS</p>
+    //     //       <p>%{ PORT }</p>
+    //     //     </body>`
+    //     //   );
+    //     // });
+    //     // // `прослушка` сервера на PORT c fn колбэк cg при успехе,провер.err
+    app.listen(PORT, (err) => {
+      // if (err) {
+      //   return console.log(err);
+      // }
+      console.log(`Сервер запущен на порту ${PORT}.....`);
+    });
+  } catch (error) {
+    //     //     // отраб.ошб.
+    //     console.log("Ошибка сервера", error.message);
+    //     //     // выход ч/з глоб.проц.мтд .exit
+    //     //     process.exit(1);
+  }
+};
+
+start();
