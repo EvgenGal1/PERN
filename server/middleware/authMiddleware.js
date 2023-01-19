38beafaa365a4b3604049712a03f1034b7875c8a
EvGen Gal <genwolk1@gmail.com>
2023-01-19 21:30:13 +0300

    authMW(декод,валид.токен), checkRoleMW(дост.ADMIN)

--- server/middleware/authMiddleware.js
+++ server/middleware/authMiddleware.js
@@ -1,18 +1,28 @@
-// const jwt = require('jsonwebtoken')
+// middlware декодер.токен,проверка валидности
 
-// module.exports = function (req, res, next) {
-//     if (req.method === "OPTIONS") {
-//         next()
-//     }
-//     try {
-//         const token = req.headers.authorization.split(' ')[1] // Bearer asfasnfkajsfnjk
-//         if (!token) {
-//             return res.status(401).json({message: "Не авторизован"})
-//         }
-//         const decoded = jwt.verify(token, process.env.SECRET_KEY)
-//         req.user = decoded
-//         next()
-//     } catch (e) {
-//         res.status(401).json({message: "Не авторизован"})
-//     }
-// };
+// подкл.ф.контролера для генерац.web токена
+const jwt = require("jsonwebtoken");
+
+module.exports = function (req, res, next) {
+  // е/и mtd OPTIONS то продолжаем
+  if (req.method === "OPTIONS") {
+    next();
+  }
+  try {
+    // достаём токен(отделяя от Типа передающ по ind 0) из шапки(обычн.там токен)
+    const token = req.headers.authorization.split(" ")[1]; // Bearer asfasnfkajsfnjk
+    // ? нужна доп.проверка по токену на авториз.
+    // if (!token) {
+    //   return res.status(401).json({ message: "Не авторизован" });
+    // }
+    // раскодир.токен. `проверять` на валидность(токен, секр.ключ)
+    const decoded = jwt.verify(token, process.env.SECRET_KEY);
+    // к запросу в поле user добав.раскодированые данн.
+    req.user = decoded;
+    // вызов след.middlware
+    next();
+  } catch (e) {
+    // проверка по токену на авториз.
+    res.status(401).json({ message: `Не авторизован. Ошибка ${e}` });
+  }
+};
--- server/middleware/checkRoleMiddleware.js
+++ server/middleware/checkRoleMiddleware.js
@@ -1,23 +1,30 @@
-// const jwt = require('jsonwebtoken')
+// middleware по добав.нов.устройство только ADMIN, +декодер,валид.
 
-// module.exports = function(role) {
-//     return function (req, res, next) {
-//         if (req.method === "OPTIONS") {
-//             next()
-//         }
-//         try {
-//             const token = req.headers.authorization.split(' ')[1] // Bearer asfasnfkajsfnjk
-//             if (!token) {
-//                 return res.status(401).json({message: "Не авторизован"})
-//             }
-//             const decoded = jwt.verify(token, process.env.SECRET_KEY)
-//             if (decoded.role !== role) {
-//                 return res.status(403).json({message: "Нет доступа"})
-//             }
-//             req.user = decoded;
-//             next()
-//         } catch (e) {
-//             res.status(401).json({message: "Не авторизован"})
-//         }
-//     };
-// }
+const jwt = require("jsonwebtoken");
+
+// экспорт fn принимающая Роль (вызов fn с передачей Роли и возврат.middleware)
+module.exports = function (role) {
+  // возвращ. сам middleware
+  return function (req, res, next) {
+    if (req.method === "OPTIONS") {
+      next();
+    }
+    try {
+      const token = req.headers.authorization.split(" ")[1]; // Bearer asfasnfkajsfnjk
+      if (!token) {
+        return res.status(401).json({ message: "Не авторизован" });
+      }
+      const decoded = jwt.verify(token, process.env.SECRET_KEY);
+      // проверка роли из токена с переданой в middleware
+      if (decoded.role !== role) {
+        return res
+          .status(403)
+          .json({ message: `Нет доступа у Роли ${decoded.role}` });
+      }
+      req.user = decoded;
+      next();
+    } catch (e) {
+      res.status(401).json({ message: `Не авторизован. Ошибка ${e}` });
+    }
+  };
+};
