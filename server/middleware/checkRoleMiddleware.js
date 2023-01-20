e1e813d41f7552aa49695572152deadaa0d26128
EvGen Gal <genwolk1@gmail.com>
2023-01-20 19:33:43 +0300

    MWcheckRole ОШБ reading 'forEach' на POSTuserPERN

--- server/middleware/checkRoleMiddleware.js
+++ server/middleware/checkRoleMiddleware.js
@@ -10,31 +10,45 @@ module.exports = function (role) {
       next();
     }
     try {
+      // достаём токен
       const token = req.headers.authorization.split(" ")[1]; // Bearer asfasnfkajsfnjk
       if (!token) {
         return res.status(401).json({ message: "Не авторизован" });
       }
-      const decoded = jwt.verify(token, process.env.SECRET_KEY);
-      // проверка роли из токена с переданой в middleware
-      // if (decoded.role !== role) {
-      if (decoded.role) {
-        if (decoded.role == "USER") {
-          return res
-            .status(403)
-            .json({ message: `Нет доступа у Роли ${decoded.role} USER` });
-        }
-        if (decoded.role !== role) {
-          return res
-            .status(403)
-            .json({ message: `Нет доступа у Роли ${decoded.role}` });
-        }
-      } else {
-        return res.status(403).json({ message: `Нет доступа` });
-      }
 
-      req.user = decoded;
+      const decoded = jwt.verify(token, process.env.SECRET_KEY);
+      req.userId = decoded._id;
+
+      // раскодир.токен.`проверять`на валидность. const опред.с др.именем т.к. role уже есть
+      // получ.масс.Ролей
+      const { roles: userRoles } = jwt.verify(token, process.env.SECRET_KEY);
+
+      // проверка масс.разреш.Ролей с разреш.для этой fn
+      // перем.для определения
+      // let hasRoles = false;
+      // // итерац.по Ролям.польз.
+      // userRoles.forEach((roles) => {
+      //   // е/и масс.разреш.Ролей содерж Роль польз.
+      //   if (role.includes(roles)) {
+      //     // перем.в true
+      //     hasRoles = true;
+      //   }
+      // });
+      let hasRole = false;
+      userRoles.forEach((roles) => {
+        if (role.includes(roles)) {
+          hasRole = true;
+        }
+      });
+      if (!hasRole) {
+        return res
+          .status(403)
+          .json({ message: `Нет доступа у Роли ${decoded.role}` });
+      }
+      // req.user = decoded;
       next();
     } catch (e) {
+      console.log(e);
       res.status(401).json({ message: `Не авторизован. Ошибка ${e}` });
     }
   };
