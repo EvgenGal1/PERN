dd03848443042f01f490968fd519bc8c05892585
EvGen Gal <genwolk1@gmail.com>
2023-01-22 23:58:23 +0300

    +model.token.user,refreshToken

---
+++ server/models/token.js
@@ -0,0 +1,18 @@
+// const { Schema, model } = require("mongoose");
+// подкл.конфиг.БД
+const { sequelize } = require("../db");
+// подкл.кл.типы полей
+const { DataTypes } = require("sequelize");
+
+// const Token = new Schema({ // mondoose
+const Token = sequelize.define("role", {
+  // ссылка на id польз.
+  user: { type: DataTypes.ObjectId, ref: "user" },
+  // `обновлять` токен созд.в БД
+  refreshToken: { type: DataTypes.STRING, required: true },
+  // ipАдрес входа, `Отпечаток пальца` браузера,..
+});
+
+// экспорт modal(резул.раб.fn) с назв.модели и схемой раб./созд.
+// model.exports = model("Token", Token);
+module.exports = Token;
