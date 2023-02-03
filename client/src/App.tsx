2b6f8be16a9b45b0368dd22f58e52604f9cb0f79
EvGen Gal <genwolk1@gmail.com>
2023-02-03 20:07:51 +0300

    ^нач.FRONT. $ CRA, TS, mobx, axios. чистка лишнего

---
+++ client/src/App.tsx
@@ -0,0 +1,12 @@
+import React, { FC } from "react";
+import "./App.css";
+
+const App: FC = () => {
+  return (
+    <div className="App">
+      123<p>321</p>0
+    </div>
+  );
+};
+
+export default App;
