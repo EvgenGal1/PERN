c7888067b15e1d6ae1aed9e34bf27188ed68bcb2
EvGen Gal <genwolk1@gmail.com>
2023-02-08 17:25:10 +0300

    api.intercep.req на 401

--- client/src/http/index.ts
+++ client/src/http/index.ts
@@ -1,5 +1,6 @@
 // ^ настр.axios
 import axios from "axios";
+import { AuthResponse } from "../models/response/auth.response";
 
 // перем.:
 export const API_URL = "http://localhost:5007/PERN/auth";
@@ -29,6 +30,34 @@ api.interceptors.request.use(
   }
 );
 
-// перехватчики.res`ответ`. 200норм. 401ошб.(нет access)>req на обнов.с refresh>е/и валид.>2 token>aces сохр.в LS>нов.res
+// перехватчики.res`ответ` на 401. 200норм. 401ошб.(нет access)>req на обнов.с refresh>е/и валид.>2 token>aces сохр.в LS>нов.res
+// use приним.2парам. 1ый calback е/и всё ОК (возвращ.config), 2ой е/и ошб.
+api.interceptors.response.use(
+  (config) => {
+    return config;
+  },
+  async (error) => {
+    // повтор исходного запроса после перезапис ACCESS (данн.для запроса) (е/и 401)
+    const originalRequest = error.config;
+    // е/и.статус код 401 приход 2 токен, перезапис ACCESS в LS. Доп.проверки config сущ., поле `повтора` не true
+    if (error.status === 401 && error.config && !error.config._isRetry) {
+      // `повтор` запроса. От повторных нов.вызовов.исход.запроса
+      originalRequest._isRetry = true;
+      try {
+        const response = await axios.get<AuthResponse>(`${API_URL}/refresh`, {
+          withCredentials: true,
+        });
+        localStorage.setItem("tokenAccess", response.data.tokens.accessToken);
+        // в экземпляр перехватчика передаём вызов исход.запроса (данн.для запроса)
+        return api.request(originalRequest);
+      } catch (error) {
+        console.log("CLT.ind.res error НЕ АВТОРИЗ. : " + error);
+      }
+    }
+    // если if не отраб., ошб.переброс на верхн.уровень
+    // return error;
+    throw error;
+  }
+);
 
 export default api;
