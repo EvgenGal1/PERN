880d15b2f9f2759e4e8298c9c8950a50d15c6cec
EvGen Gal <genwolk1@gmail.com>
2023-05-02 22:01:53 +0300

    SRV.Tok. Отдельные routes на JS

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,15 +1,15 @@
 // ^ Tok. Маршрутизатор всех маршрутов приложения (подобие allRoutes)
 import express from "express";
-// import product from "./product.routes.js";
-import category from "./category.routes.js";
-// import brand from "./brand.routes.js";
-// import user from "./user.routes.js";
+import product from "./product.js";
+import category from "./category.js";
+import brand from "./brand.js";
+import user from "./user.js";
 
 const router = new express.Router();
 
-// router.use("/product", product);
+router.use("/product", product);
 router.use("/category", category);
-// router.use("/brand", brand);
-// router.use("/user", user);
+router.use("/brand", brand);
+router.use("/user", user);
 
 export default router;
