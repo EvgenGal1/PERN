5c0177f1f517cb6d4fdbe4bb7074164231fe113e
EvGen Gal <genwolk1@gmail.com>
2023-05-21 17:27:10 +0300

    SRV.Tok. +login(ctrl,serv), cg. Работает

--- server/controllers/User.js
+++ server/controllers/User.js
@@ -3,6 +3,9 @@ import jwt from "jsonwebtoken";
 import UserModel from "../services/User.js";
 import AppError from "../error/AppError_Tok.js";
 
+// ! дописать cntrl,services для login и signup
+import { User as UserMapping } from "../models/mapping.js";
+
 const makeJwt = (id, email, role) => {
   return jwt.sign({ id, email, role }, process.env.SECRET_KEY, {
     expiresIn: "24h",
@@ -10,10 +13,13 @@ const makeJwt = (id, email, role) => {
 };
 
 class User {
+  // Регистрация
   async signup(req, res, next) {
+    console.log("SRV.ctrl.User signup ", 1);
     // res.status(200).send("Регистрация пользователя");
     const { email, password, role = "USER" } = req.body;
     try {
+      console.log("SRV.ctrl.User signup try ", 2);
       if (!email || !password) {
         throw new Error("Пустой email или пароль");
       }
@@ -25,15 +31,25 @@ class User {
       const token = makeJwt(user.id, user.email, user.role);
       return res.json({ token });
     } catch (e) {
+      console.log("SRV.ctrl.User signup catch ", 2);
+      console.log("SRV.ctrl.User signup catch e ", e);
       next(AppError.badRequest(e.message));
     }
   }
 
+  // Вход
   async login(req, res, next) {
+    console.log("SRV.ctrl.User login ", 1);
     // res.status(200).send("Вход в личный кабинет");
     try {
+      console.log("SRV.ctrl.User login try ", 2);
       const { email, password } = req.body;
-      const user = await UserModel.getByEmail(email);
+      console.log("SRV.ctrl.User login email ", email);
+      console.log("SRV.ctrl.User login password ", password);
+
+      // const user = await UserModel.getByEmail(email);
+      // ! дописать cntrl,services для login и signup
+      const user = await UserModel.login(email /* , password */);
       let compare = bcrypt.compareSync(password, user.password);
       if (!compare) {
         throw new Error("Указан неверный пароль");
@@ -41,6 +57,8 @@ class User {
       const token = makeJwt(user.id, user.email, user.role);
       return res.json({ token });
     } catch (e) {
+      console.log("SRV.ctrl.User login catch ", 2);
+      console.log("SRV.ctrl.User login catch e ", e);
       next(AppError.badRequest(e.message));
     }
   }
