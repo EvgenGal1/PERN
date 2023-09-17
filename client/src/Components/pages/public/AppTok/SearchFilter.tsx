dc2396f8c619d69c0a4db580c11b12de45187b9b
EvGen Gal <genwolk1@gmail.com>
2023-09-17 21:51:34 +0300

    СЛН.CLT. Search (тесты, мн.cg)

--- client/src/Components/pages/public/AppTok/SearchFilter.tsx
+++ client/src/Components/pages/public/AppTok/SearchFilter.tsx
@@ -22,6 +22,26 @@ let defaultIdCatalog: any = {
 
 const SearchFilter = (/* { show, setShow , children }: any */) => {
   const { catalog } = useContext(AppContext);
+  console.log("FILTER    000 catalog ", catalog);
+  console.log("FILTER    111 catalog.categories ", catalog.categories);
+  console.log("FILTER    022 catalog.category ", catalog.category);
+  console.log(
+    "FILTER    333 catalog.categories[catalog.category] ",
+    catalog.categories[catalog.category]
+  );
+  console.log(
+    "FILTER    444 catalog.categories[Number - catalog.category] ",
+    catalog.categories[Number(catalog.category)]
+  );
+  console.log("catalog.categories.id ", catalog.categories.id);
+  console.log(
+    "FILTER    555 catalog.categories.id = catalog.category ",
+    (catalog.categories.id = catalog.category)
+  );
+  // console.log(
+  //   "catalog.categories[Number(catalog.category)].name ",
+  //   catalog.categories[Number(catalog.category)].name
+  // );
 
   const navigate = useNavigate();
 
@@ -39,10 +59,23 @@ const SearchFilter = (/* { show, setShow , children }: any */) => {
     "FILTER    000 overwrittenСatalogParamsFromStore ",
     overwrittenСatalogParamsFromStore
   );
-  // е/и в cataloge что-то есть (str|id параметра)
+  // е/и в cataloge что-то есть (str|id параметры)
   if (catalog.category || catalog.brand) {
+    // let catCategory,
+    //   catBrand = "";
+    // let catCategory = catalog.category;
+    // let catBrand = catalog.brand;
+    // console.log("catCategory | catBrand 000 ", catCategory, "|", catBrand);
     // category есть и не записанные
     if (catalog.category && overwrittenСatalogParamsFromStore.category) {
+      // itemIdStat.category = catalog.category;
+      // catCategory = catalog.category;
+      // catalog.category = "";
+      //
+      // itemIdStat.category = catCategory;
+      // catCategory = "";
+      //
+      //
       // запись str|id из парам.catalog Магазина в state itemIdStat один раз от повтор.перезаписи
       itemIdStat.category = catalog.category;
       setOverwrittenCatalogParamsFromStore(
@@ -57,30 +90,53 @@ const SearchFilter = (/* { show, setShow , children }: any */) => {
 
       let nameCategory = "";
       for (let nameSpl of splitCat) {
+        console.log("nameSpl ", nameSpl);
         // eslint-disable-next-line no-loop-func
         catalog.categories.map((item: any) => {
+          console.log("item ", item);
           // if (item.id === Number(catalog.category)) {
           if (item.id === Number(nameSpl)) {
             // if (item.id === Number(splitCat)) {
+            console.log("12345 item.name ", 12345, item.name);
+            console.log("nameCategory <<>> ", nameCategory);
             if (nameCategory === "") {
+              console.log("1 ", 1);
               nameCategory = item.name;
             } else {
+              console.log("2 ", 2);
               nameCategory = nameCategory + "_" + item.name;
             }
+            console.log("nameCategory === ", nameCategory);
             valueNameStat.category = nameCategory;
           }
           return nameCategory;
           // Array.prototype.map () ожидает возврата от функции стрелки
         });
       }
+      //
+
+      for (var i = 0; i < splitCat.length; i++) {}
+
+      // Function declared in a loop contains unsafe references to variable(s) 'nameCategory', 'nameCategory'
+      // Функция, объявленная в цикле, содержит небезопасные ссылки на переменные (ы) «Namecategory», «NameCategory»
+      // console.log("nameCategory ", nameCategory);
+      // valueNameStat.category = nameCategory;
     }
     if (catalog.brand && overwrittenСatalogParamsFromStore.brand) {
+      // itemIdStat.brand = catalog.brand;
+      // catBrand = catalog.brand;
+      // catalog.brand = "";
+      //
+      // itemIdStat.brand = catBrand;
+      // catBrand = "";
+      //
       itemIdStat.brand = catalog.brand;
       setOverwrittenCatalogParamsFromStore(
         (overwrittenСatalogParamsFromStore.brand = false)
       );
     }
   }
+  // console.log("catCategory | catBrand === ", catCategory, "|", catBrand);
 
   // блок./разблок.прокрутки body при modal Расшир.Поиске
   let body = document.querySelector("body");
@@ -117,17 +173,35 @@ const SearchFilter = (/* { show, setShow , children }: any */) => {
 
     const itemId = item.id;
     const itemName = item.name;
+    // console.log("FLT itemId itemName item ", itemId, itemName, item);
+    // id: 2; name: "Сердца"
 
     const eventCheck = event.target.checked;
     const eventName = event.target.name;
     const eventValue = event.target.value;
+    // console.log(
+    //   "FLT check name value event ",
+    //   eventCheck,
+    //   eventName,
+    //   eventValue,
+    //   event
+    // );
+    // event.target.value - Сердца, Custom, Atlant, Горои и т.д.
+    // event.target.name - brand, category и т.д.
+    // event.target.checked
 
     let valueNameElem = dataValueName[eventName];
     let itemIdElem = dataItemId[eventName];
+    console.log("FLT valueNameElem ", valueNameElem, "=");
+    console.log("FLT itemIdElem ", itemIdElem, "=");
+    // "", "Сердца_Горои", "Custom" || "", "2_5", "1"
 
+    // if (eventCheck) {
+    // console.log("check 000  ", eventCheck);
     // ^ написать fn по проверке/вставке/замене чего либо (str<>num<>obj) в чём либо (str<>arr<>obj). принимает 3+1 парам.(что, в чём, fn(ревёрс, удал, добав, наличие), ? разделитель)
     if (valueNameElem !== "")
       if (!valueNameElem.includes("_")) {
+        console.log("FLT без _  ", 0);
         valueNameElem =
           eventValue === valueNameElem ? "" : valueNameElem + "_" + eventValue;
         itemIdElem =
@@ -135,6 +209,7 @@ const SearchFilter = (/* { show, setShow , children }: any */) => {
             ? ""
             : itemIdElem + "_" + itemId;
       } else if (valueNameElem.includes(eventValue)) {
+        console.log("FLT includes значение  ", 1);
         valueNameElem = valueNameElem.match(`(?<=_)${eventValue}`)
           ? valueNameElem.replace("_" + eventValue, "")
           : valueNameElem.replace(eventValue + "_", "");
@@ -142,13 +217,18 @@ const SearchFilter = (/* { show, setShow , children }: any */) => {
           ? itemIdElem.replace("_" + itemId, "")
           : itemIdElem.replace(itemId + "_", "");
       } else {
+        console.log("FLT есть _, нет Id ", 3);
         valueNameElem = valueNameElem + "_" + eventValue;
         itemIdElem = itemIdElem + "_" + itemId;
       }
     else {
+      console.log("FLT пусто ", 101);
       valueNameElem = eventValue;
       itemIdElem = String(itemId);
     }
+    // }
+    console.log("FLT dataValueName[eventName] ", dataValueName[eventName]);
+    console.log("FLT valueNameElem === ", valueNameElem);
 
     dataValueName[eventName] = valueNameElem;
     setValueNameStat(dataValueName);
@@ -169,6 +249,9 @@ const SearchFilter = (/* { show, setShow , children }: any */) => {
     // if (catalog.limit !== (20 || 0 || 10000)) params.limit = catalog.limit;
     // if (catalog.sortOrd !== ("ASC" || null)) params.sortOrd = catalog.sortOrd;
     // if (catalog.sortField !== ("name" || null)) params.sortField = catalog.sortField;
+
+    // console.log("FILTER    catalog ", catalog);
+    // console.log("FILTER    params ", params);
 
     // при наличии (category,brand) отправка на URL /catalog/list/ иначе главная
     if (catalog.brand || catalog.category) {
