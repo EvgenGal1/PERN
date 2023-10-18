dc6f44f2349ee95852027b63603f09d520da7740
EvGen Gal <genwolk1@gmail.com>
2023-10-18 23:39:48 +0300

    СЛН.CLT. UpdOrd(врем.обнвл.и откл.req к БД)

--- client/src/Components/layout/AppTok/UpdateOrder.tsx
+++ client/src/Components/layout/AppTok/UpdateOrder.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable react/jsx-pascal-case */
 // ^ модальн.окно редактирование Заказа
 import { useState, useEffect } from "react";
 import uuid from "react-uuid";
@@ -11,7 +12,7 @@ import {
 } from "../../../http/Tok/orderAPI_Tok";
 import UpdateItems from "./UpdateItems";
 import Modal__eg from "../../ui/Modal/Modal__eg";
-import FormField__eg from "../../ui/Form/FormField__eg";
+import FormFieldRecursive__EG from "../../ui/Form/FormFieldRecursive__EG";
 
 const defaultValue = {
   name: "",
@@ -35,6 +36,7 @@ const defaultItems = {
   unique: "",
 };
 
+// fn валид.
 const isValid = (value: any) => {
   const result: any = {};
   const pattern = /^[1-9][0-9]*$/;
@@ -58,10 +60,10 @@ const isValid = (value: any) => {
       result.address =
         // pattern.test(value.address);
         value.address.trim() !== "";
-    // if (key === "comment")
-    //   result.comment =
-    //     // pattern.test(value.comment);
-    //     value?.comment /* ?.trim() */ /* || " " */;
+    if (key === "comment")
+      result.comment =
+        // pattern.test(value.comment);
+        value?.comment /* ?.trim() */ /* || " " */;
   }
   return result;
 };
@@ -112,55 +114,111 @@ const updateItems = async (items: any, orderId: any) => {
   }
 };
 
-const UpdateOrder = (props: any) => {
+// типы
+interface Props {
+  id: number | string;
+  show: boolean;
+  // setShow: (show: boolean) => void;
+  setChange: (change: boolean) => void;
+  setShow: any; // (e: ChangeEvent<HTMLInputElement>) => void;
+  orders: any; // { [key: string]: string | number | string[] };
+}
+interface FormErrors {
+  name: string | null;
+  email?: null | string;
+  phone?: null | string;
+  address?: null | string;
+  comment?: null | string;
+  sms?: string;
+  password?: string;
+}
+
+const UpdateOrder: React.FC<Props> = (props) => {
   const { id, show, setShow, setChange, orders } = props;
 
-  const [value, setValue] = useState(defaultValue);
-  const [valid, setValid] = useState(defaultValid);
+  const order2 = {
+    name: orders.name.toString(),
+    email: orders.email.toString(),
+    phone: orders.phone.toString(),
+    address: orders.address.toString(),
+    comment: orders?.comment == null ? "" : orders?.comment.toString(),
+  };
+  // данн.Польз-ля по Заказу
+  const [value, setValue] = useState(order2);
 
-  const [items, setItems] = useState([]);
-  //   { id: "", name: "", price: "", quantity: "", unique: "" },
+  let ordItmsMap = orders.items.map((item: any) => {
+    // при добавлении новой хар-ки свойство append принимает значение true
+    // при изменении старой хар-ки свойство change принимает значение true
+    // при удалении старой хар-ки свойство remove принимает значение true
+
+    let itemMap = Object.entries(item);
+    itemMap = itemMap.concat([
+      ["unique", uuid()],
+      ["append", false],
+      ["remove", false],
+      ["change", false],
+    ]);
+
+    return itemMap;
+  });
+
+  // Позиции Заказа
+  const [items, setItems] = useState(
+    // { id: "", name: "", price: "", quantity: "", unique: "" }
+    ordItmsMap
+  );
+
+  // валидация/ошибки
+  const [valid /* formErrors */, setValid /* setFormErrors */] = useState(
+    /* defaultValid */
+    /* isValid(order2) */
+    /* name: "", // null
+    email: "", // null
+    phone: "", // null
+    address: "", // null
+    comment: "", // null */
+    order2
+  );
+
+  // ^ пока не нужнй код. пробы обн.данн.ч/з Род.Комп.Order
+  // useEffect(() => {
+  //   // if (id) {  adminGetOne(id).then((data) => {получ.данн.Товара с БД}) } // ^ упразднено
+  //   //     // получ.данн.Товара с БД
+  //   const order = {
+  //     name: orders.name.toString(),
+  //     email: orders.email.toString(),
+  //     phone: orders.phone.toString(),
+  //     address: orders.address.toString(),
+  //     comment: orders?.comment == null ? "" : orders?.comment.toString(),
+  //   };
+  //   setValue(order);
+  //   setValid(isValid(order));
+  //   // setValid(order);
+  //   // для удобства работы с хар-ми зададим для каждой уникальный идентификатор и доп.свойства, которые подскажут нам, какой http-запрос на сервер нужно выполнить — добавления, обновления или удаления характеристики
+  //   setItems(
+  //     orders.items.map((item: any) => {
+  //       // при добавлении новой хар-ки свойство append принимает значение true
+  //       // при изменении старой хар-ки свойство change принимает значение true
+  //       // при удалении старой хар-ки свойство remove принимает значение true
+  //       return {
+  //         ...item,
+  //         unique: uuid(),
+  //         append: false,
+  //         remove: false,
+  //         change: false,
+  //       };
+  //     })
+  //   );
+  // }, [
+  //   orders.name,
+  //   orders.email,
+  //   orders.phone,
+  //   orders.address,
+  //   orders.comment,
+  //   orders.items,
   // ]);
 
-  useEffect(() => {
-    // if (id) {  adminGetOne(id).then((data) => {получ.данн.Товара с БД}) } // ^ упразднено
-    //     // получ.данн.Товара с БД
-    const order = {
-      name: orders.name.toString(),
-      email: orders.email.toString(),
-      phone: orders.phone.toString(),
-      address: orders.address.toString(),
-      comment: orders?.comment == null ? "" : orders?.comment.toString(),
-    };
-    setValue(order);
-    setValid(isValid(order));
-    setValid(order);
-    // для удобства работы с хар-ми зададим для каждой уникальный идентификатор и доп.свойства, которые подскажут нам, какой http-запрос на сервер нужно выполнить — добавления, обновления или удаления характеристики
-    setItems(
-      orders.items.map((item: any) => {
-        // при добавлении новой хар-ки свойство append принимает значение true
-        // при изменении старой хар-ки свойство change принимает значение true
-        // при удалении старой хар-ки свойство remove принимает значение true
-        return {
-          ...item,
-          unique: uuid(),
-          append: false,
-          remove: false,
-          change: false,
-        };
-      })
-    );
-  }, [
-    orders.name,
-    orders.email,
-    orders.phone,
-    orders.address,
-    orders.comment,
-    orders.items,
-  ]);
-
-  const itemsId = items;
-  const amount: any = itemsId.reduce(
+  const amount: any = orders.items.reduce(
     (sum: number, item: { price: number; quantity: number }) =>
       sum + item.price * item.quantity,
     0
@@ -169,23 +227,20 @@ const UpdateOrder = (props: any) => {
   const handleInputChange = (event: any) => {
     const data = { ...value, [event.target.name]: event.target.value };
     setValue(data);
-    setValid(isValid(data));
+    // setValid(isValid(data));
   };
 
+  // ^ врем.откл.req к БД
   const handleSubmit = async (event: any) => {
     // event.preventDefault();
-    console.log("UpdateOrder handleSubmit event ", event);
     /*
      * На первый взгляд кажется, что переменная correct не нужна, можно обойтись valid, но это не так. Нельзя использовать значение valid сразу после изменения этого значения — ф-ция setValid не изменяет значение состояния мгновенно. Вызов функции лишь означает — React «принял к сведению» наше сообщение, что состояние нужно изменить.
      */
     const correct = isValid(value);
     setValid(correct);
 
-    console.log("UpdateOrder handleSubmit value ", value);
-
     // если введенные данные прошли проверку — можно отправлять их на сервер
     if (correct.name && correct.email && correct.phone && correct.address) {
-      console.log("UpdOrd Sbmt IF correct ", 123);
       const data = new FormData();
       data.append("name", value.name.trim());
       data.append("email", value.email.trim());
@@ -197,7 +252,6 @@ const UpdateOrder = (props: any) => {
       if (items.length) {
         await updateItems(items, id);
       }
-
       // adminUpdate(id, data)
       //   .then((data) => {
       //     const order = {
@@ -238,19 +292,38 @@ const UpdateOrder = (props: any) => {
   return (
     //eslint-disable-next-line react/jsx-pascal-case
     <Modal__eg
-      closureBoundary={true}
+      // closureBoundary={true}
       isOpen={show}
       onClose={setShow}
       header={header}
       body={
         <>
+          <>
+            {/* // ^ Комп.FFR (ток на масс.) */}
+            {/* // eslint-disable-next-line react/jsx-pascal-case */}
+            <FormFieldRecursive__EG
+              valueArr={[
+                ["name", value.name],
+                ["address", value.address],
+                [
+                  ["email", value.email],
+                  ["phone", value.phone],
+                ],
+                ["comment", value.comment],
+              ]}
+              handleChange={handleInputChange}
+              formClass={"mt-3"}
+              label={true}
+              legend={"Recursive -  Пользователь"}
+            />
+          </>
+          {/* // ^ Комп.FF (масс.(1ый)(не управ.inpt в > влож.)/объ.(2ой)(раздел.renrder на не/объедин.блоки)) */}
+          {/* <div className="uniteddiv df df-row"> */}
           {/*  eslint-disable-next-line react/jsx-pascal-case */}
-          <FormField__eg
+          {/* <FormField__eg
             // handleSubmit={handleSubmit}
             handleChange={handleInputChange}
             valueArr={[
-              ["name", value.name],
-              ["address", value.address],
               [
                 ["phone", value.phone],
                 ["email", value.email],
@@ -259,66 +332,42 @@ const UpdateOrder = (props: any) => {
             ]}
             valid={valid}
             label={true}
-            union={["phone", "email"]}
+            unionObj={["phone", "email"]}
             // nonField={true}
             legend={"на массиве -  Пользователь"}
-          />
+            clForm={"mt-3"}
+          /> */}
           {/*  eslint-disable-next-line react/jsx-pascal-case */}
           {/* <FormField__eg
             // handleSubmit={handleSubmit}
             handleChange={handleInputChange}
-            valueObj={{
-              name: value.name,
+            valueObj={{ 
               phone: value.phone,
-              email: value.email,
-              address: value.address,
+              email: value.email, 
               comment: value.comment,
             }}
             valid={valid}
             label={true}
-            union={["phone", "email"]}
+            unionObj={["phone", "email"]}
             // nonField={true}
             legend={"на объекте - Пользователь"}
-            clT={"mt-3"}
+            clForm={"mt-3"}
           /> */}
-          <div className="uniteddiv df df-row">
-            {/*  eslint-disable-next-line react/jsx-pascal-case */}
-            {/* <FormField__eg
-              handleChange={handleInputChange}
-              valueObj={{
-                phone: value.phone,
-                email: value.email,
-                comment: value.comment,
-              }}
-              valid={valid}
-              union={["phone", "email"]}
-              legend={"Котакты"}
-              clT={"mt-3"}
-              label={true}
-              // nonField={true}
-            /> */}
-            {/*  eslint-disable-next-line react/jsx-pascal-case */}
-            {/* <FormField__eg
-              handleChange={handleInputChange}
-              valueObj={{
-                // phone: value.phone,
-                // email: value.email,
-                comment: value.comment,
-              }}
-              valid={valid}
-              label={true}
-              union={[]}
-              legend={"Комментрарии"}
-              clT={"mt-3 ml-4"}
-              // nonField={true}
-            /> */}
-          </div>
+          {/* </div> */}
           {/* Позиции  */}
-          <div className="mt-5">
+          <div className="">
             <UpdateItems items={items} setItems={setItems} />
           </div>
           <div className="df df-row df-jcsb df-aic mt-4">
-            <button type="submit" className="btn--eg btn-success--eg">
+            {/* // ^ сделать проверку на ошб. validationArr/formErrors здесь а не только в FormFieldRecursive */}
+            <button
+              type="submit"
+              className="btn--eg btn-success--eg"
+              // блок.кнп. е/и пусты name/address или есть ошб.
+              // disabled={
+              //   valueArr[0][1] === "" || valueArr[1][1] === "" || !isFormValid
+              // }
+            >
               Сохранить
             </button>
             <div>
