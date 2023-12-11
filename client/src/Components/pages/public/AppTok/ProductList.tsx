e343ca1479de74aa3d143751a5abf75c2ac4b790
EvGen Gal <genwolk1@gmail.com>
2023-12-12 02:49:52 +0300

    ProductList(перенос из Shop usEf,загр.,усл.ренд.)

--- client/src/Components/pages/public/AppTok/ProductList.tsx
+++ client/src/Components/pages/public/AppTok/ProductList.tsx
@@ -1,16 +1,68 @@
-import { useState, useContext } from "react";
+import { useState, useContext, useEffect } from "react";
 import { observer } from "mobx-react-lite";
+import { useLocation, useSearchParams } from "react-router-dom";
+import { Spinner } from "react-bootstrap";
 
+import { fetchAllProducts } from "../../../../http/Tok/catalogAPI_Tok";
 import { AppContext } from "../../../layout/AppTok/AppContext";
-import ProductItem from "./ProductItem";
 import { PaginSortLimit } from "../../../layout/AppTok/PaginSortLimit";
+import { getSearchParams } from "../../../../scripts/helpers/getSearchParams";
+import ProductItem from "./ProductItem";
 
 const ProductList = observer(() => {
   const { catalog }: any = useContext(AppContext);
 
+  const location = useLocation();
+  const [searchParams] = useSearchParams();
+
+  // признак загрузки данных. // ! врем.откл.
+  const [productsFetching, setProductsFetching] = useState(true);
+
   // врем.заглушки для PaginSortLimit
   const [fetching, setFetching] = useState(false);
   const [change, setChange] = useState(false);
+
+  useEffect(() => {
+    console.log("ProdLs usEf 000 ", 0);
+    const { category, brand, page, limit, sortOrd, sortField } =
+      getSearchParams(searchParams);
+
+    console.log("ProdLs usEf location ", location);
+
+    catalog.category = category;
+    catalog.brand = brand;
+    catalog.page = page ?? 1;
+    catalog.limit = limit ?? 0;
+    catalog.sortOrd = sortOrd ?? "ASC";
+    catalog.sortField = sortField ?? "name";
+
+    const fetchData = async () => {
+      try {
+        const data = await fetchAllProducts(
+          catalog.category,
+          catalog.brand,
+          catalog.page,
+          catalog.limit,
+          catalog.sortOrd,
+          catalog.sortField
+        );
+        // ! ошб.е/и пропис.true выше - Недостаточно памятидля загрюстр.
+        setProductsFetching(true);
+        console.log("ProdLs usEf PRD data ", data);
+        catalog.products = data.rows;
+        catalog.limit = Math.ceil(data.limit);
+        catalog.count = data.count;
+      } catch (error) {
+        console.error("Ошибка загрузки Продуктов:", error);
+      } finally {
+        setProductsFetching(false);
+      }
+    };
+
+    fetchData();
+
+    // eslint-disable-next-line
+  }, [location.search]);
 
   return (
     <>
@@ -39,13 +91,24 @@ const ProductList = observer(() => {
         ) : (
           <p className="m-3">По вашему запросу ничего не найдено</p>
         )} */}
-        {catalog.products.length ? (
-          catalog.products.map((item: any) => (
-            <ProductItem key={item.id} data={item} />
-          ))
-        ) : (
-          <p className="m-3">По вашему запросу ничего не найдено</p>
-        )}
+        {/*  */}
+        <>
+          {productsFetching ? (
+            <div style={{ textAlign: "center" }}>
+              <Spinner animation="border" variant="danger" />
+            </div>
+          ) : (
+            <>
+              {catalog.products.length ? (
+                catalog.products.map((item: any) => (
+                  <ProductItem key={item.id} data={item} />
+                ))
+              ) : (
+                <p className="m-3">По вашему запросу ничего не найдено</p>
+              )}
+            </>
+          )}
+        </>
       </div>
       {/* ПАГИНАЦИЯ | СОРТИРОВКА | ЛИМИТ */}
       {catalog.products.length ? (
