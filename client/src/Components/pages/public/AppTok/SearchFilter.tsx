dbb11d765634e9a18830362e0f82584e936a6dae
EvGen Gal <genwolk1@gmail.com>
2023-12-12 02:53:20 +0300

    SearchFilter(перенос fn()findValStrArr>общ.helper)

--- client/src/Components/pages/public/AppTok/SearchFilter.tsx
+++ client/src/Components/pages/public/AppTok/SearchFilter.tsx
@@ -8,6 +8,7 @@ import {
   SHOP_CATALOG_ROUTE,
   FILTER_ROUTE,
 } from "../../../../utils/consts";
+import { findValueFromStringInArray } from "../../../../scripts/helpers/findValueFromStringInArray";
 
 // перем. Имён, Id, фильтров из Каталога, кол-ва Товаров
 let defaultValueName: any = { category: "", brand: "", name: "", price: "" };
@@ -19,27 +20,6 @@ let defaultIdCatalog: any = {
   price: true,
 };
 let countProduct: number | string = "";
-
-// ^ написать fn (в helperы) по проверке/вставке/замене чего либо (str<>num<>obj) в чём либо (str<>arr<>obj). принимает 3+1 парам.(что, в чём, fn(ревёрс, удал, добав, наличие), ? разделитель)
-// запись name в state по знач.стр. поиск name в масс.объ. по совпадению id и значения из строки и подтягивания name в отд.перем. ч/з разделитель
-export function findValueFromStringInArray(string: string, array: any) {
-  // перем. разбития строка на части, масс.Имён
-  const digits = string.split("_");
-  const names: any[] = [];
-  // перебор масс.по кол-ву знач.в строке
-  for (let i = 0; i < digits.length; i++) {
-    const digit = parseInt(digits[i]);
-    // ищем объект с соответствующим id
-    const matchingObject = array.find((obj: any) => obj.id === digit);
-    // е/и есть соответ. и такого имени ещё нет, добав.name в масс
-    if (matchingObject && !names.includes(matchingObject.name)) {
-      names.push(matchingObject.name);
-    }
-  }
-  // соед.имена ч/з разделитель
-  const result = names.join("_");
-  return result;
-}
 
 const SearchFilter = () => {
   const { catalog } = useContext(AppContext);
