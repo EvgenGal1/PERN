b6239ff18d3d980adf607ecc8cf12f435a91bd7b
EvGen Gal <ZBst.1@yandex.ru>
2025-01-02 10:21:11 +0300

    SRV. # правки MW > аутен/авториз/админ

--- server/middleware/adminMiddleware.ts
+++ server/middleware/adminMiddleware.ts
@@ -1,41 +1,21 @@
 // ^ проверка Роли ADMIN для доп.прав
-import { Request, Response, NextFunction } from "express";
+import { Request, Response, NextFunction } from 'express';
 
-import AppError from "../error/ApiError";
-import { DecodedToken /* , CustomRequest */ } from "../types/DecodedToken";
+import AppError from '../error/ApiError';
+import { DecodedToken } from '../types/DecodedToken';
 
-// ! ошб. - Свойство "role" не существует в типе "DecodedToken"
-// ^ объявил в authMiddleware
-// interface DecodedToken {
-//   role: string;
-//   // добавьте другие свойства, если они есть в вашем токене
-// }
-// ! ошб. - Последовательные объявления свойств должны иметь один и тот же тип. Свойство "auth" должно иметь тип "DecodedToken", но имеет здесь тип "DecodedToken"
-// ^ объявил в authMiddleware
-// declare global {
-//   namespace Express {
-//     interface Request {
-//       auth?: DecodedToken;
-//     }
-//   }
-// }
-
-// ^ объедин.interface
-// interface DecodedToken {
-//   role: string;
-// }
 interface CustomRequest extends Request {
   auth?: DecodedToken;
 }
 
 const admin = (req: CustomRequest, res: Response, next: NextFunction): void => {
   try {
-    if (req.auth?.role !== "ADMIN") {
-      throw new Error("Только для администратора");
+    if (req.auth?.role !== 'ADMIN') {
+      throw new Error('Только для администратора');
     }
     next();
-  } catch (e) {
-    next(AppError.forbidden(e.message));
+  } catch (error: unknown) {
+    next(AppError.forbidden((error as Error).message));
   }
 };
 
--- server/middleware/authMiddleware.ts
+++ server/middleware/authMiddleware.ts
@@ -1,10 +1,9 @@
 // ^ подтвержд.личности ч/з JWT-токена полученый либо после регистрации, либо после входа в личный кабинет
-import { Request, Response, NextFunction } from "express";
-import jwt from "jsonwebtoken";
+import { Request, Response, NextFunction } from 'express';
 
-import AppError from "../error/ApiError";
-import { DecodedToken /*, CustomRequest */ } from "../types/DecodedToken";
-import TokenService from "../services/token.service";
+import AppError from '../error/ApiError';
+import { DecodedToken /*, CustomRequest */ } from '../types/DecodedToken';
+import TokenService from '../services/token.service';
 
 interface CustomRequest extends Request {
   auth?: DecodedToken;
@@ -12,35 +11,35 @@ interface CustomRequest extends Request {
 
 const auth = (req: CustomRequest, res: Response, next: NextFunction): void => {
   // е/и mtd OPTIONS то продолжаем (проверка GET,POST,и т.д.)
-  if (req.method === "OPTIONS") {
+  if (req.method === 'OPTIONS') {
     next();
   }
   try {
     // провер header на наличие поля authorization
     const authorizationHeader = req.headers.authorization;
     if (!authorizationHeader) {
-      return next(AppError.unauthorizedError("Требуется авторизация"));
+      return next(AppError.unauthorizedError('Требуется авторизация'));
     }
 
     // достаём токен из header (отделяя от типа `Носитель` передающегося по ind 0) из шапки(обычн.там токен)
-    const accessToken = authorizationHeader?.split(" ")[1]; // Bearer token (asfasnfkajsfnjk)
+    const accessToken = authorizationHeader?.split(' ')[1]; // Bearer token (asfasnfkajsfnjk)
     if (!accessToken) {
-      return next(AppError.unauthorizedError("Токен  отсутствует"));
+      return next(AppError.unauthorizedError('Токен  отсутствует'));
     }
 
     // раскодир.токен. `проверять` валидации ч/з services (токен)
     const decoded = TokenService.validateAccessToken(
-      accessToken
+      accessToken,
     ) as DecodedToken;
-    if (!decoded) return next(AppError.forbidden("Токен не валиден"));
+    if (!decoded) return next(AppError.forbidden('Токен не валиден'));
 
     // к запросу в поле user добав.раскодированые данн.
     req.auth = decoded;
 
     // вызов след.middlware
     next();
-  } catch (e) {
-    next(AppError.forbidden(e.message));
+  } catch (error: unknown) {
+    next(AppError.forbidden((error as Error).message));
   }
 };
 
