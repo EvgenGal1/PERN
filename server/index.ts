c42a0acf08589795fcfb4b90632df87d07f728c2
EvGen Gal <ZBst.1@yandex.ru>
2025-01-02 10:08:54 +0300

    SRV. # index (подкл./правки swg, тесты, env, др)

--- server/index.ts
+++ server/index.ts
@@ -1,29 +1,31 @@
 // ^ Запуск Сервера. Базов.конфиг для приёма запросов
 
 // express ч/з require для прилож.
-import express from "express";
+import express from 'express';
 // ф.наст./перем.конфигураций
 // import config from "dotenv/config";
 // ^ от ошб.для TS - [nodemon] clean exit - code: '28P01',length: 117,severity: '�����'
-require("dotenv").config({ path: "./.env" });
+require('dotenv').config({ path: './.env' });
 // cors > отправ.запр.с брауз.
-import cors from "cors";
+import cors from 'cors';
 // загрузчик файлов
-import fileUpload from "express-fileupload";
+import fileUpload from 'express-fileupload';
 // MW по корр.раб. с cookie
-import cookieParser from "cookie-parser";
+import cookieParser from 'cookie-parser';
 
 // схема БД
 // import * as model from "./models/model";
 // конфиг.БД
-import sequelize from "./sequelize";
+import sequelize from './sequelize';
 // MW jsoобраб.ошб.
-import ErrorHandler from "./middleware/ErrorHandler";
+import ErrorHandler from './middleware/ErrorHandler';
 // общ.ф.настр.маршрутизаторов
-import router from "./routes/index.routes";
+import router from './routes/index.routes';
+// док./настр. swagger фай
+import { documentSwagger } from './swagger.config';
 
 // порт из перем.окруж. | умолч.
-const PORT = process.env.PORT_PERN || 5000;
+const PORT = process.env.SRV_PORT || 5000;
 
 // созд.server
 const app = express();
@@ -35,20 +37,23 @@ app.use(
     // указ.с каким domen обмен.cookie{разреш.cok.,url front}
     {
       credentials: true,
-      origin: process.env.CLIENT_URL_CLN,
-    }
-  )
+      origin: process.env.CLT_URL,
+    },
+  ),
 );
 // MiddleWare возм.парсить json
 app.use(express.json());
 // MW для статики (img, css)
-app.use(express.static(/*path.resolve(__dirname,*/ "static") /*)*/);
+app.use(`/${process.env.PUB_DIR}`, express.static(`${process.env.PUB_DIR}`));
 // MW для загрузки файлов
 app.use(fileUpload());
 // MW для раб.с cookie
 app.use(cookieParser(process.env.SECRET_KEY));
 // обраб./прослуш. все маршруты приложения. 1ый str. префикс пути(/api), 2ой подкл.Маршрутизатор(MW)
-app.use("/api", router);
+app.use('/api', router);
+
+// документирование (Swagger)
+documentSwagger(app);
 
 // обработка ошибок
 app.use(ErrorHandler);
@@ -63,11 +68,16 @@ const start = async () => {
       // if (erorr) {
       //   return console.log(erorr);
       // }
-      console.log(`Сервер PERN (СЛН) запущен на порту, ${PORT}`);
+      console.log(
+        `MAIN   SRV: ${process.env.SRV_URL}   DB: ${process.env.DB_NAME}:${process.env.DB_PORT}`,
+      );
     });
   } catch (erorr) {
-    console.log(erorr);
+    console.log('m.err : ', erorr);
   }
 };
 
-start();
+// start() при прямом запуске > изоляции сервера при тестах
+if (require.main === module) start();
+
+export default app;
