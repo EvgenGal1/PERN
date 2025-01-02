d416f2f41f3aa7e6f435bc0bd1a024230b49e151
EvGen Gal <ZBst.1@yandex.ru>
2025-01-02 06:35:07 +0300

    SRV. # Sequelize (env, коммент);  + gitignore

--- server/sequelize.ts
+++ server/sequelize.ts
@@ -1,28 +1,27 @@
 // ^ настройки подключения к серверу БД (sequelize | db)
-import Sequelize from "sequelize";
+import Sequelize from 'sequelize';
 
 export default new Sequelize(
-  process.env.DB_NAME_PERN /* Tok */, // база данных
+  process.env.DB_NAME, // база данных
   process.env.DB_USER, // пользователь
-  process.env.DB_PASS_PERN, // пароль
+  process.env.DB_PSW, // пароль
   {
     dialect: process.env.DB_USER,
     host: process.env.DB_HOST,
     port: process.env.DB_PORT,
     define: {
-      // использовать snake_case вместо camelCase для полей таблиц БД. вкл.поля
+      // вкл. snake_case вместо camelCase > назв.полей БД
       underscored: true,
-      // timestamps: false, // не добавлять поля created_at и updated_at при создании таблиц
+      // вкл.поля created_at и updated_at
       timestamps: true,
       // вкл. `createdAt`
       createdAt: true,
       // вкл. `updatedAt`
       updatedAt: true,
-      // Изменяем название `updatedAt`
+      // измен.назв.`updatedAt`
       // updatedAt: 'updateTimestamp',
     },
-    // ^ Формат даты заказа. 1ый способ. ? Нужно для 2го ?
-    logging: false, // ? не перезаписывает ? Таблицы (нет лога созд.Табл.)
-    timezone: "Europe/Moscow",
-  }
+    logging: false, // без лог.записей
+    timezone: 'Europe/Moscow',
+  },
 );
