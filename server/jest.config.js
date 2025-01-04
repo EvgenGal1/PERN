e35f5a7ebf8873b3cbe8f1e015cfd23440e145cf
EvGen Gal <ZBst.1@yandex.ru>
2025-01-04 23:44:09 +0300

    SRC. # ts/jest config (расширение, улучшения)

--- server/jest.config.js
+++ server/jest.config.js
@@ -1,4 +1,16 @@
 module.exports = {
-  preset: 'ts-jest',
-  testEnvironment: 'node',
+  preset: 'ts-jest', // поддержка TS
+  testEnvironment: 'node', // среда выполнения
+  transform: {
+    '^.+\\.tsx?$': 'ts-jest', // преобразов.ф.с расшир. .ts/tsx
+  },
+  // игнор.пути
+  testPathIgnorePatterns: ['/node_modules/', '/dist/'],
+  globals: {
+    'ts-jest': {
+      tsconfig: 'tsconfig.json', // конфиг
+    },
+  },
+  testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.ts$', //  регул.выраж. > нахождения тестов
+  moduleFileExtensions: ['ts', 'js', 'json', 'node'], // поддержка расширений
 };
