b985878923c5192ea7c49c60469bdd14a8784b6e
EvGen Gal <ZBst.1@yandex.ru>
2025-01-06 05:14:56 +0300

    SRV. + нов/обнов.ico; # index (^ лог.jest, # logg,swg,seqiliz,ErrHand)

--- server/src/index.ts
+++ server/src/index.ts
@@ -10,9 +10,10 @@ import cors from 'cors';
 import fileUpload from 'express-fileupload';
 // MW по корр.раб. с cookie
 import cookieParser from 'cookie-parser';
+import path from 'path';
 
 // конфиг.БД
-import sequelize from './config/sequelize';
+import sequelize, { connectToDatabase } from './config/sequelize';
 // общ.ф.настр.маршрутизаторов
 import router from './routes/index.routes';
 // MW обраб.ошб.
@@ -20,11 +21,11 @@ import ErrorHandler from './middleware/errors/ErrorHandler';
 // логирование LH Winston
 import { LoggingWinston as logger } from './config/logging/log_winston.config';
 // MW логгирование входящих HTTP запросов
-import { loggingMiddleware } from './middleware/logging.middleware';
+import { loggingMiddleware as loggerMW } from './middleware/logging.middleware';
 // документирование/настр. Swagger
 import { documentSwagger } from './config/documents/swagger.config';
 // константы > команды запуска process.env.NODE_ENV
-import { isDevelopment } from './config/envs/env.consts';
+import { isDevelopment, isProduction } from './config/envs/env.consts';
 
 // загр.перем.окруж.из опред.ф.env
 config({ path: `.env.${process.env.NODE_ENV}` });
@@ -35,6 +36,7 @@ const NODE_ENV = process.env.NODE_ENV || 'development';
 const app = express();
 // порт из перем.окруж. | умолч.
 const PORT = process.env.SRV_PORT;
+const PUB_DIR = process.env.PUB_DIR || 'public';
 
 // совместн.использ.ресурс.разн.источников client/server > разрещ.cookie опред.url
 app.use(cors({ credentials: true, origin: process.env.CLT_URL }));
@@ -42,13 +44,13 @@ app.use(cors({ credentials: true, origin: process.env.CLT_URL }));
 app.use(cookieParser(process.env.SECRET_KEY));
 // MW возм.парсить json
 app.use(express.json());
+// MW логг.Winston вход.HTTP req
+app.use(loggerMW);
 // MW > стат.ф. (img, css)
-app.use(`/${process.env.PUB_DIR}`, express.static(`${process.env.PUB_DIR}`));
+app.use(express.static(path.join(__dirname, `../${PUB_DIR}`)));
 // MW > загр.ф.
 app.use(fileUpload());
 
-// логгирование (Winston) входящих HTTP запросов
-app.use(loggingMiddleware);
 // обраб./прослуш. всех маршр.приложения (путь, Маршрутизатор)
 app.use(`/${process.env.SRV_NAME}`, router);
 // тест.маршрут
@@ -66,15 +68,12 @@ const start = async () => {
   try {
     // подкл.к БД.
     await sequelize.authenticate();
+    // проверка подкл.к БД
+    if (isDevelopment) await connectToDatabase();
     // синхрониз.структуру БД со схемой данн.(опред.моделью)
     await sequelize.sync();
     // цвета запуска: DEV - зелённый, PROD - синий
-    const mainColor = isDevelopment ? '\x1b[32m' : '\x1b[34m'; // Проверка значений переменных окружения
-    if (!process.env.SRV_URL || !process.env.DB_NAME || !process.env.DB_PORT) {
-      console.warn(
-        'ОШИБКА: одна или несколько переменных окружения не установлены!',
-      );
-    }
+    const mainColor = isDevelopment ? '\x1b[32m' : '\x1b[34m';
     // прослуш.подключ.PORT и fn()callback
     app.listen(PORT, () => {
       // вывод с цветом подкл.к БД от NODE_ENV
@@ -97,6 +96,7 @@ const start = async () => {
 };
 
 // start() при прямом запуске > изоляции сервера при тестах
-if (isDevelopment && require.main === module) start();
+if (isProduction) start();
+else if (isDevelopment && require.main === module) start();
 // экспорт приложения > тестов
 export default app;
