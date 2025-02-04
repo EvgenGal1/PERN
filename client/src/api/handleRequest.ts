5419dbdfc9143039e50870ab6edd6da25a7168fe
EvGen Gal <ZBst.1@yandex.ru>
2025-02-05 01:50:09 +0300

    CLT. + handleRequest (обраб.req API, - кэш)

---
+++ client/src/api/handleRequest.ts
@@ -0,0 +1,46 @@
+// ^ обработчик API запросов к БД
+
+// кэш
+// import { queryCache } from "../utils/cache";
+// обраб.ошб.
+import { errorHandler } from "../utils/errorHandler";
+
+type ApiMethod<T> = () => Promise<{ data: T }>;
+
+/**
+ * Обрабатывает API-запросы, автоматически обрабатывая ошибки
+ * @param method - Метод возвращающий Promise с Axios-ответом
+ * @param context - Контекст для логирования (название метода)
+ */
+export const handleRequest = async <T>(
+  method: ApiMethod<T>,
+  context: string = "Unknown"
+  // cacheKey?: string,
+  // ttl: number = 60_000
+): Promise<T> => {
+  // обраб.req/ошб.
+  try {
+    // кэш проверка/возврат
+    // if (cacheKey) {
+    //   const cached = queryCache.get<T>(cacheKey);
+    //   if (cached) return cached;
+    // }
+    // вызов мтд.
+    const response = await method();
+    const result = response.data;
+    // кэш запись
+    // if (cacheKey) queryCache.set(cacheKey, result, ttl);
+    // возврат данн.
+    return result;
+  } catch (error) {
+    // кэш удал.запись по регул.выраж.ключей
+    // if (cacheKey) queryCache.invalidate(new RegExp(cacheKey));
+    // обраб.ошб.
+    const processedError = errorHandler(error, context);
+    // логг.ошб.с контекстом
+    // logger.error(error);
+    console.error(`handleRequest [${context}] ОШБ: `, processedError);
+    // выброс.ошб.
+    throw processedError;
+  }
+};
