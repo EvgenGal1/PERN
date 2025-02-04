91e7104744d5a86e727f62ac8f29a7ec0fcd44de
EvGen Gal <ZBst.1@yandex.ru>
2025-02-05 01:43:31 +0300

    CLT. +^ доп.клс.обраб.ошб.; дораб.основ.обраб.ошб.

--- client/src/utils/errorHandler.ts
+++ client/src/utils/errorHandler.ts
@@ -1,21 +1,35 @@
-// ^ обработчик ошибок запросов(axios/req/res) к БД
+// ^ основной обработчик ошибок (axios/req/res c БД, спец.доп.ошб., натив.ошб.JS, неизвестные ошб.)
 
 import { AxiosError } from "axios";
 // логгирование
 // import * as Sentry from "@sentry/react";
 
-import { logout } from "../api/auth/authAPI";
-import { ErrorRes } from "../Components/ErrorBoundary/ErrorBoundary";
+import { APIError } from "./errorClasses";
+import { authAPI } from "../api/auth/authAPI";
 
-export const errorHandler = (error: unknown): ErrorRes => {
-  // Логирование в Sentry
-  // Sentry.captureException(error);
+export interface ApiError {
+  status: number;
+  message: string;
+  code: string;
+  errors?: Record<string, unknown>;
+}
+
+export const errorHandler = (error: unknown, context?: string): ApiError => {
+  // баз.объ.ошб.
+  const baseError = {
+    status: 500,
+    message: "Неизвестная ошибка",
+    code: "UNKNOWN_ERROR",
+  };
+  // логг.Sentry
+  // if (process.env.NODE_ENV === "production") Sentry.captureException(error, { tags: { context } });
+  // Логирование контекста
+  console.error(`errorHandler [${context}] ОШБ: `, error);
 
   // обраб.ошб.axios
   if (error instanceof AxiosError) {
-    // ошб.без ответа сервера
+    // ошб.сетевые (нет ответа)
     if (!error.response) {
-      console.error("ApiError Network Err : ", error.message);
       return {
         status: 503,
         message: "Сервер недоступен",
@@ -23,17 +37,16 @@ export const errorHandler = (error: unknown): ErrorRes => {
       };
     }
 
+    // ошб.от БД
     const { status, data } = error.response;
-    const responseData = data as {
-      message?: string;
-      errors?: Record<string, unknown>;
-      code?: string;
-    };
+    const responseData = data as ApiError;
 
     // при 401 Выход Пользователя и редирект на Авторизацию
     if (status === 401) {
-      logout();
+      authAPI.logout();
       window.location.href = "auth/login";
+      // localStorage.removeItem("tokenAccess");
+      // window.location.href = "/login";
     }
 
     return {
@@ -44,20 +57,19 @@ export const errorHandler = (error: unknown): ErrorRes => {
     };
   }
 
-  // обраб.ошб. Error
-  if (error instanceof Error) {
-    // объ.ошб.
+  // спец.доп.ошб.
+  if (error instanceof APIError) {
     return {
-      status: 500,
+      status: error.status,
       message: error.message,
-      code: "INTERNAL_ERROR",
+      code: error.code,
+      errors: error.errors,
     };
   }
 
+  // натив.ошб.JS
+  if (error instanceof Error) return { ...baseError, message: error.message };
+
   // Неизвестные ошибки
-  return {
-    status: 500,
-    message: "Неизвестная ошибка",
-    code: "UNKNOWN_ERROR",
-  };
+  return baseError;
 };
