236c4a70229f618b3e2b431b4b2d8b600625b495
EvGen Gal <ZBst.1@yandex.ru>
2025-03-11 11:44:09 +0300

    SRV. ^ rename п. public <> static

--- server/src/index.ts
+++ server/src/index.ts
@@ -40,7 +40,7 @@ const NODE_ENV = process.env.NODE_ENV || 'development';
 const app: Application = express();
 // порт из перем.окруж. | умолч.
 const PORT = Number(process.env.SRV_PORT) || 5000;
-const PUB_DIR = process.env.PUB_DIR || 'public';
+const PUB_DIR = process.env.PUB_DIR || 'static';
 
 // совместн.использ.ресурс.разн.источников client/server > разрещ.(url,cookie)
 app.use(
