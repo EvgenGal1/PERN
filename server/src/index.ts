fb0f05f1df08b5cd64d304c317a38b75b1366c48
EvGen Gal <ZBst.1@yandex.ru>
2025-03-19 04:40:01 +0300

    SRV. # index > - ! Vercel

--- server/src/index.ts
+++ server/src/index.ts
@@ -142,7 +142,11 @@
 
 import express, { Request, Response } from 'express';
 const app = express();
+const PORT = process.env.PORT || 5000;
 app.get('/', (req: Request, res: Response) => {
   res.send('Hello Vercel');
+});
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
 export default app;
