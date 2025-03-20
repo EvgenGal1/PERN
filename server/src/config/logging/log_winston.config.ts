8cbbc8262483a87d09a7aae6ec4bc4244b04edbb
EvGen Gal <ZBst.1@yandex.ru>
2025-03-21 01:41:41 +0300

    SRV. ^# logger(-п.isProduction), vercel(-includeFiles) > - ! Vercel

--- server/src/config/logging/log_winston.config.ts
+++ server/src/config/logging/log_winston.config.ts
@@ -6,12 +6,13 @@ import path from 'path';
 import fs from 'fs';
 
 // перем.окруж.
-import { isProduction, isDevelopment } from '../envs/env.consts';
+import { isDevelopment } from '../envs/env.consts';
 
 // путь п.логов DEV/PROD
-const logDir = path.join(process.cwd(), isDevelopment ? 'tmp/logs' : 'logs');
+const logDir = isDevelopment ? path.join(process.cwd(), 'tmp/logs') : null;
 // проверка/созд.п. > логов
-if (!fs.existsSync(logDir)) fs.mkdirSync(logDir, { recursive: true });
+if (isDevelopment && logDir)
+  if (!fs.existsSync(logDir)) fs.mkdirSync(logDir, { recursive: true });
 
 // форматы логирования
 const { colorize, timestamp, combine, json, printf, errors } = format;
@@ -36,29 +37,33 @@ export const LoggingWinston = createLogger({
     // логи в консоль
     new transports.Console(),
     // детал.настр.общ.ф.логов > кажд.день
-    new transports.DailyRotateFile({
-      filename: path.join(logDir, 'application-%DATE%.log'),
-      datePattern: 'YYYY-MM-DD',
-      zippedArchive: true, // архив.логи
-      maxFiles: '14d', // дней хранений
-      maxSize: '20m', // размер ф.в MB
-    }),
-    // доп.ф.error.log > лог.ошб.
-    new transports.File({
-      filename: path.join(logDir, 'error.log'),
-      level: 'error',
-    }),
+    ...(isDevelopment && logDir
+      ? [
+          new transports.DailyRotateFile({
+            filename: path.join(logDir, 'application-%DATE%.log'),
+            datePattern: 'YYYY-MM-DD',
+            zippedArchive: true, // архив.логи
+            maxFiles: '14d', // дней хранений
+            maxSize: '20m', // размер ф.в MB
+          }),
+          // доп.ф.error.log > лог.ошб.
+          new transports.File({
+            filename: path.join(logDir, 'error.log'),
+            level: 'error',
+          }),
+        ]
+      : []),
   ],
 });
 
 // логг. > PROD
-if (isProduction) {
-  LoggingWinston.add(
-    new transports.Http({
-      // отправка логов в сторонний сервис
-      host: 'log-service.example.com',
-      path: '/logs',
-      ssl: true,
-    }),
-  );
-}
+// if (isProduction) {
+//   LoggingWinston.add(
+//     new transports.Http({
+//       // отправка логов в сторонний сервис
+//       host: 'log-service.example.com',
+//       path: '/logs',
+//       ssl: true,
+//     }),
+//   );
+// }
