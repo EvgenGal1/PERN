b1ca4d413cadb43cc3d40f448c6e818391fff4ae
EvGen Gal <ZBst.1@yandex.ru>
2025-06-11 02:56:49 +0300

    SRV. # улучш.roleMW > roles[]

--- server/src/middleware/auth/roleMiddleware.ts
+++ server/src/middleware/auth/roleMiddleware.ts
@@ -4,12 +4,12 @@ import ApiError from '../errors/ApiError';
 
 const roleMW = (requiredRoles: string[]) => {
   return (req: Request, res: Response, next: NextFunction): void => {
-    const userRole = req.auth?.role;
+    const userRole = req.auth?.roles[0];
     if (!userRole) {
       return next(ApiError.forbidden('Роль Пользователя не найдена'));
     }
 
-    if (!requiredRoles.includes(userRole)) {
+    if (!requiredRoles.includes(userRole.role)) {
       return next(ApiError.forbidden(`Доступ для роли ${userRole} запрещен`));
     }
 
