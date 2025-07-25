c121c171f42eeb14babea4e66525689ee0e5a705
EvGen Gal <ZBst.1@yandex.ru>
2025-07-26 00:59:25 +0300

    CLT. ^#+ axios/API/cookie (улучш.req/res, пропуск res.ошб.auth, прям.req.refresh, очистка Токены/cookie)

--- client/src/api/axiosInstances.ts
+++ client/src/api/axiosInstances.ts
@@ -6,18 +6,18 @@ import axios, {
   InternalAxiosRequestConfig,
 } from "axios";
 
-import UserStore from "@/store/UserStore";
+import { deleteCookie, getCookie } from "@/utils/cookies";
 
 // Базовый URL API
 const API_URL = process.env.REACT_APP_SRV_IURL;
 
-// 1. `гостевой экземпляр` axios (неавториз.польз., отправ.cookie в кажд.req(register,login,activate,PasswordReset))
+// 1. `гостевой экземпляр` axios (неавториз.польз., отправ.cookie в кажд.req ч/з withCredentials)
 const guestInstance = axios.create({ baseURL: API_URL, withCredentials: true });
 
 // 2. авторизованный экземпляр axios (провереные пользователи, отправ.cookie в кажд.req)
 const authInstance = axios.create({ baseURL: API_URL, withCredentials: true });
 
-// Перехватчик > добав.Токена в заголовки авториз.req
+// настр.Перехватчика > добав.Токена в заголовки авториз.req
 const authInterceptor = (
   config: InternalAxiosRequestConfig
 ): InternalAxiosRequestConfig => {
@@ -25,38 +25,54 @@ const authInterceptor = (
   if (token) config.headers.authorization = `Bearer ${token}`;
   return config;
 };
-// добав.Перехватчик > авториз.req
+// в Перехватчик REQ + настр.headers > авториз.req
 authInstance.interceptors.request.use(authInterceptor);
 
-// Перехватчик > возврат.успех(cd-200-успех) / обраб.ошб.(401/нет access), обнов.Токена, повтор req
+// Перехватчик RES > возврат.успех(cd-200-успех) / обраб.ошб.(401/проблемы с req/res.auth), обнов.Токена, повтор req
 authInstance.interceptors.response.use(
   (response: AxiosResponse) => response,
   async (error: AxiosError) => {
     // приведение(as)/расширение(&) типа нач.req > предовращ.повторных req
-    const initialReq = error.config as InternalAxiosRequestConfig & {
+    const originalRequest = error.config as InternalAxiosRequestConfig & {
       _isRetry?: boolean;
     };
+    // опред.req.auth
+    const isAuthRequest = originalRequest.url?.startsWith("auth/");
+    // пропуск.ВСЕ req.auth от бесконечных req/res // пропуск проверки.user и обнов.токена - originalRequest.url?.includes("auth/check") || + refresh
+    if (isAuthRequest) return Promise.reject(error);
 
     // ошб.401 и не повторный req (Токен обнов.в БД/сохр.в LS)
-    if (error.response?.status === 401 && initialReq && !initialReq._isRetry) {
+    if (error.response?.status === 401 && !originalRequest._isRetry) {
       // пометка > предовращ. повторных req
-      initialReq._isRetry = true;
+      originalRequest._isRetry = true;
 
       try {
-        // req обнов.Токена
-        await UserStore.prototype.refreshToken();
+        // req.обнов.Токена ч/з guestInstance (без interceptor/header и без рекурс.req с authInstance, cookie автозацеп.ч/з withCredentials)
+        const { data } = await guestInstance.post("auth/refresh", null, {
+          headers: {
+            "Content-Type": "application/json",
+            // Явно передаем refreshToken из кук
+            Cookie: `tokenRefresh=${getCookie("tokenRefresh")}`,
+          },
+        });
+        // сохр.нов.Access Токен
+        localStorage.setItem("tokenAccess", data.tokenAccess);
+        // обнов.нов.Refresh Токен (откл. обнов.ч/з Backend.cookie)
+        // setCookie('tokenRefresh', data.tokenRefresh, COOKIE_OPTIONS);
         // ч/з экземпл.перехватчика повтор нач.req с нов.Токеном
-        return authInstance.request(initialReq);
+        return authInstance(originalRequest);
       } catch (error: unknown) {
-        console.error("Ошибка при обновлении токена:", error);
-        // удал.Токена при неудаче
+        console.error("Ошибка при req.auth:", error);
+        // при ошб.req.auth удал.все Токены и cookie, редирект на вход
         localStorage.removeItem("tokenAccess");
+        deleteCookie("tokenRefresh");
+        deleteCookie("basketId");
         window.location.href = "/login";
-        throw error;
+        return Promise.reject(error);
       }
     }
     // проброс не авториз.ошб.дальше
-    throw error;
+    return Promise.reject(error);
   }
 );
 // экспорт.экземп.axios
