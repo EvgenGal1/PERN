c6989f03d60c88ca9bff405d68aa3579231ce832
EvGen Gal <ZBst.1@yandex.ru>
2025-08-01 00:03:05 +0300

    SRV. ~# SWG - ! Vercel

--- server/src/config/documents/swagger.config.ts
+++ server/src/config/documents/swagger.config.ts
@@ -96,8 +96,8 @@ export const documentSwagger = (app: Application): void => {
       // customCss: combinedCss,
       // объедин.стили Базовый/Кастомный в ф.
       // customCssUrl: `${process.env.SRV_URL}/${process.env.PUB_DIR}/swagger/combined.css`,
-      customCssUrl: `${process.env.SRV_URL}/swagger/combined.css`,
-      // customCssUrl: `${process.env.SRV_URL}/swagger/theme.css`,
+      // customCssUrl: `${process.env.SRV_URL}/swagger/combined.css`, // ^ раб.совмещ.варик
+      customCssUrl: `${process.env.SRV_URL}/swagger/theme.css`,
       // customCssUrl: `${process.env.SRV_URL}/${process.env.PUB_DIR}/swagger/theme.css`,
       // customCssUrl: `/${process.env.PUB_DIR}/swagger/theme.css`,
       // customCssUrl: `../../../public/swagger/theme.css`,
