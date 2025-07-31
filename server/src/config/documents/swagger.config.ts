e5fab3cbe6d178dade0c387c05444d63eb6104ce
EvGen Gal <ZBst.1@yandex.ru>
2025-07-31 21:18:44 +0300

    SRV. ~# SWG - ! Vercel

--- server/src/config/documents/swagger.config.ts
+++ server/src/config/documents/swagger.config.ts
@@ -85,8 +85,8 @@ export const documentSwagger = (app: Application): void => {
         // url: '/swagger',
       },
       // кастом.иконки в браузере
-      customfavIcon: `/${process.env.PUB_DIR}/swagger/icon.ico`, //  ^ вроде все раб - проверил было - опять пропало
-      // customfavIcon: `../../../${process.env.PUB_DIR}/swagger/icon.ico`, // ! не раб
+      // customfavIcon: `/${process.env.PUB_DIR}/swagger/icon.ico`, //  ^ вроде все раб - проверил было - опять пропало
+      customfavIcon: `../../../${process.env.PUB_DIR}/swagger/icon.ico`, // ! не раб
       // customfavIcon: `${process.env.SRV_URL}/${process.env.PUB_DIR}/swagger/icon.ico`, //  ^ вроде все раб
       // кастом ф.CSS
       // customCss: `${process.env.SRV_URL}/swagger/theme.css`, // загр.ток.свои стили
@@ -98,8 +98,9 @@ export const documentSwagger = (app: Application): void => {
       // объедин.стили Базовый/Кастомный
       // customCss: combinedCss,
       // объедин.стили Базовый/Кастомный в ф.
-      customCssUrl: `${process.env.SRV_URL}/${process.env.PUB_DIR}/swagger/combined.css`,
+      // customCssUrl: `${process.env.SRV_URL}/${process.env.PUB_DIR}/swagger/combined.css`,
       // customCssUrl: `${process.env.SRV_URL}/${process.env.PUB_DIR}/swagger/theme.css`,
+      customCssUrl: `../../../${process.env.PUB_DIR}/swagger/theme.css`,
       // кастом ф.JS (для отраб.статич.ф.на PROD - Vercel)
       customJs: [
         'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui-bundle.min.js',
