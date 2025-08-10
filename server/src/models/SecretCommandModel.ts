f7e98bcfc5ebb910760fa2ddf0c2d29e71a74b2c
EvGen Gal <ZBst.1@yandex.ru>
2025-08-10 23:15:45 +0300

    SRV. +^ МЕГА ДОП. нов.табл.SecretCommand > кмд./комбинаций/нажатий с расширением доступов/логики/возможностей

---
+++ server/src/models/SecretCommandModel.ts
@@ -0,0 +1,88 @@
+import {
+  DataTypes,
+  Model,
+  InferAttributes,
+  InferCreationAttributes,
+  Sequelize,
+} from 'sequelize';
+
+import { Models } from 'src/types/models.interfaсe';
+
+class SecretCommandModel extends Model<
+  InferAttributes<SecretCommandModel>,
+  InferCreationAttributes<SecretCommandModel>
+> {
+  declare id: number;
+  declare name: string;
+  declare description: string | null;
+  declare keyCombination: 'sequence' | 'simultaneous' | 'touchpad';
+  declare requiredRole: string;
+  declare requiredLevel: number;
+  declare isActive: boolean;
+  declare createdAt: Date;
+  declare updatedAt: Date;
+
+  static associate(models: Models) {}
+
+  static initModel(sequelize: Sequelize) {
+    SecretCommandModel.init(
+      {
+        id: {
+          type: DataTypes.INTEGER,
+          autoIncrement: true,
+          primaryKey: true,
+        },
+        name: {
+          type: DataTypes.STRING,
+          allowNull: false,
+          unique: true,
+        },
+        description: {
+          type: DataTypes.TEXT,
+          allowNull: true,
+        },
+        keyCombination: {
+          type: DataTypes.JSONB,
+          allowNull: false,
+        },
+        requiredRole: {
+          type: DataTypes.STRING,
+          allowNull: false,
+          field: 'required_role',
+        },
+        requiredLevel: {
+          type: DataTypes.INTEGER,
+          allowNull: false,
+          defaultValue: 1,
+          field: 'required_level',
+        },
+        isActive: {
+          type: DataTypes.BOOLEAN,
+          allowNull: false,
+          defaultValue: true,
+          field: 'is_active',
+        },
+        // переопред.поля timestamps от настр. авто NotNull в sequelize.timestamps
+        createdAt: {
+          type: DataTypes.DATE,
+          allowNull: false,
+          field: 'created_at',
+        },
+        updatedAt: {
+          type: DataTypes.DATE,
+          allowNull: true, // разреш.null
+          defaultValue: null,
+          field: 'updated_at',
+        },
+      },
+      {
+        sequelize,
+        tableName: 'secret_commands', // имя таблицы в БД
+        // modelName: 'SecretCommand', // имя модели в коде (опционально)
+        timestamps: false, // откл.вр.метки т.к. устан.вручную
+      },
+    );
+  }
+}
+
+export default SecretCommandModel;
