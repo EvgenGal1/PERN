d1cbbc21f924fef2e3b46c88f2fe3dd43ef3361f
EvGen Gal <ZBst.1@yandex.ru>
2025-08-12 01:50:16 +0300

    SRV. +^ SecretCommand.serv с мтд.получ.доступных кмд./комбин.

---
+++ server/src/services/secretCommand.service.ts
@@ -0,0 +1,42 @@
+import { Op } from 'sequelize';
+
+import SecretCommandModel from '../models/SecretCommandModel';
+import { RoleLevels } from '../types/role.interface';
+
+interface FormattedCommand {
+  name?: string;
+  keys: string[];
+  type: 'sequence' | 'simultaneous' | 'touchpad';
+}
+
+class SecretCommandService {
+  /** получ.масс. кмд./комбин. доступных Пользователю по его Роли/Уровню */
+  async getAvailableCommands(
+    userRoles: RoleLevels[],
+  ): Promise<FormattedCommand[]> {
+    // е/и нет Ролей, нет и кмд./комбин.
+    if (!userRoles || userRoles.length === 0) return [];
+
+    // настр.услов.ч/з ИЛИ где кмд./комбин. <= по Уровню в своёй Роли
+    const orConditions = userRoles.map((role) => ({
+      requiredRole: role.role,
+      requiredLevel: { [Op.lte]: role.level },
+    }));
+
+    // req кмд./комбин.из БД
+    const commands = await SecretCommandModel.findAll({
+      where: { isActive: true, [Op.or]: orConditions },
+      attributes: ['name', 'keyCombination'],
+    });
+
+    // формат.данн.под Front по типу - "availableCommands": [ { "name": "dov_menu_on", "keys": ["d","o","p","m","n"], "type": "sequence" }, ... ]
+    const formattedCommands: FormattedCommand[] = commands.map((cmd) => {
+      const combo = cmd.keyCombination as FormattedCommand;
+      return { name: cmd.name, keys: combo.keys, type: combo.type };
+    });
+
+    return formattedCommands;
+  }
+}
+
+export default new SecretCommandService();
